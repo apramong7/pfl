8ef9b42a6424213121f0be91263138ad0173dba9
AnaPaulaRamon <apramong@uwaterloo.ca>
2023-07-24 00:04:00 -0400

    changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -21,9 +21,9 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.5 : 1.15}
+        position={isMobile ? [0, -3, -2.2] : [0.3, -3.25, 0.01]}
+        rotation={[-0.03, 2.2, 0.01]}
       />
     </mesh>
   );
@@ -54,24 +54,28 @@ const ComputersCanvas = () => {
   }, []);
 
   return (
-    <Canvas
-      frameloop='demand'
-      shadows
-      dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Computers isMobile={isMobile} />
-      </Suspense>
+    <>
+    {isMobile
+    ? <p>Hello there</p>
+    : <Canvas
+    frameloop='demand'
+    shadows
+    dpr={[1, 2]}
+    camera={{ position: [20, 3, 5], fov: 25 }}
+    gl={{ preserveDrawingBuffer: true }}
+  >
+    <Suspense fallback={<CanvasLoader />}>
+      <OrbitControls
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+      />
+      <Computers isMobile={isMobile} />
+    </Suspense>
 
-      <Preload all />
-    </Canvas>
+    <Preload all />
+  </Canvas>}
+    </>
   );
 };
 
